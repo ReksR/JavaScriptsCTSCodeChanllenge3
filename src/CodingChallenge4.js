41d2a67e94acfb030bdb7563ad5e2f92ae0e3b49
devopsrekha <61942349+devopsrekha@users.noreply.github.com>
2020-07-05 12:02:53 +0530

    edited html file

--- src/CodingChallenge4.js
+++ src/CodingChallenge4.js
@@ -1,29 +1,27 @@
 const Mark = {
-  fullName : "Mark Nichole" ,
-  Mass : 80,
-  height: 1.40,
-  BMI: function () {
-    this.BMI = (this.Mass/(this.height*this.height))
-    return this.BMI
+  fullName: "Mark Nichole",
+  Mass: 80,
+  height: 1.4,
+  BMI: function() {
+    this.BM = this.Mass / (this.height * this.height);
+    return this.BM;
   }
-}
-
+};
 
 const John = {
-  fullName : "Mark Nichole" ,
-  Mass : 90,
-  height: 1.90,
-  BMI: function () {
-    this.BMI = (this.Mass/(this.height*this.height)) 
-    return this.BMI
+  fullName: "Mark Nichole",
+  Mass: 90,
+  height: 1.9,
+  BMI: function() {
+    this.BM = this.Mass / (this.height * this.height);
+    return this.BM;
   }
+};
+
+if (Mark.BMI() < John.BMI()) {
+  console.log("Mark is Healthy");
+} else {
+  console.log("john is healthy");
 }
 
-
-if (Mark.BMI()<John.BMI()){
-  console.log("Mark is Healthy")
-}else{
- console.log( "john is healthy")
-}
-
-console.log(Mark,John)
+console.log(Mark, John);
